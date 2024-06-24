3c44baa1c7e4fccf20ee09b4d995ca644a33fc8b
Duy Phong <nguyenduyphong1504@gmail.com>
2024-06-24 11:10:26 +0700

    create project

---
+++ src/main/java/likelion/backend/happyrestaurant/HappyRestaurantApplication.java
@@ -0,0 +1,13 @@
+package likelion.backend.happyrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/likelion/backend/happyrestaurant/HappyRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.backend.happyrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
